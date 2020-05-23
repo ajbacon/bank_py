866bbdb667dedc1730e9aff423d5263597758ecb
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-23 18:25:00 +0100

    feat: add Transaction class, test it has amount property

---
+++ test/test_transaction.py
@@ -0,0 +1,25 @@
+import unittest
+import os.path
+import sys
+sys.path.append(os.path.abspath('.'))
+
+from lib.transaction import Transaction
+
+
+class TestTransaction(unittest.TestCase):
+
+    def setUp(self):
+        self.credit = Transaction(1000)
+
+    def test_amount(self):
+        """should have a property to return the amount"""
+        self.assertEqual(self.credit.amount, 1000)
+
+    # def test_type_credit(self):
+    #     """should have a type property of 'CREDIT' when a cre property to return the amount"""
+    #     self.assertEqual(credit.amount, 1000)
+    #     self.assertEqual(credit.type, 'CREDIT')
+
+
+if __name__ == "__main__":
+    unittest.main()
--- test/transaction.py
+++
@@ -1,6 +0,0 @@
-import unittest
-import os.path
-import sys
-sys.path.append(os.path.abspath('.'))
-
-from lib.bank_account import BankAccount
