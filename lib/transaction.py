c4f08cdddd1fc060adc171b2e4169848f09e9d42
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-23 18:34:48 +0100

    feat: add category to Transaction class with test

--- lib/transaction.py
+++ lib/transaction.py
@@ -1,7 +1,12 @@
 class Transaction():
-    def __init__(self, amount):
+    def __init__(self, amount, category):
         self._amount = amount
+        self._category = category
 
     @property
     def amount(self):
         return self._amount
+
+    @property
+    def category(self):
+        return self._category
--- test/test_transaction.py
+++ test/test_transaction.py
@@ -9,16 +9,16 @@ from lib.transaction import Transaction
 class TestTransaction(unittest.TestCase):
 
     def setUp(self):
-        self.credit = Transaction(1000)
+        self.credit = Transaction(1000, 'CREDIT')
 
     def test_amount(self):
         """should have a property to return the amount"""
         self.assertEqual(self.credit.amount, 1000)
 
-    # def test_type_credit(self):
-    #     """should have a type property of 'CREDIT' when a cre property to return the amount"""
-    #     self.assertEqual(credit.amount, 1000)
-    #     self.assertEqual(credit.type, 'CREDIT')
+    def test_type_credit(self):
+        """should have a type property of 'CREDIT' when a cre property to return the amount"""
+        self.assertEqual(self.credit.amount, 1000)
+        self.assertEqual(self.credit.category, 'CREDIT')
 
 
 if __name__ == "__main__":
