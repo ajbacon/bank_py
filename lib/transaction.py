0f4552222437f5caf7e0ed5d6a741bc8440c7320
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-24 11:24:08 +0100

    feat: update and store balance after a credit transaction, included tests

--- lib/transaction.py
+++ lib/transaction.py
@@ -1,7 +1,8 @@
 class Transaction():
-    def __init__(self, amount, category):
+    def __init__(self, amount, category, old_balance):
         self._amount = amount
         self._category = category
+        self._balance = self.__update_balance(amount, old_balance)
 
     @property
     def amount(self):
@@ -10,3 +11,12 @@ class Transaction():
     @property
     def category(self):
         return self._category
+
+    @property
+    def balance(self):
+        return self._balance
+
+    # private methods
+    def __update_balance(self, amount, old_balance):
+        self._balance = amount + old_balance
+        return self._balance
--- test/test_transaction.py
+++ test/test_transaction.py
@@ -9,19 +9,23 @@ from lib.transaction import Transaction
 class TestTransaction(unittest.TestCase):
 
     def setUp(self):
-        self.credit = Transaction(1000, 'CREDIT')
-        self.debit = Transaction(500, 'DEBIT')
+        self.credit = Transaction(1000, 'CREDIT', 500)
+        self.debit = Transaction(500, 'DEBIT', 1000)
 
     def test_amount(self):
-        """should have a property to return the amount"""
+        """it should have a property to return the amount"""
         self.assertEqual(self.credit.amount, 1000)
 
     def test_category_credit(self):
-        """should have a property of category, which is set to either 'CREDIT' or 'DEBIT'"""
+        """it should have a property of category, which is set to either 'CREDIT' or 'DEBIT'"""
         self.assertEqual(self.credit.amount, 1000)
         self.assertEqual(self.credit.category, 'CREDIT')
         self.assertEqual(self.debit.amount, 500)
         self.assertEqual(self.debit.category, 'DEBIT')
+
+    def test_update_balance_credit(self):
+        """it should update and store the new balance after a credit transaction"""
+        self.assertEqual(self.credit.balance, 1500)
 
 
 if __name__ == "__main__":
