cc160dbc4f56a892cd203ba5854602b28fc78700
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-24 08:11:05 +0100

    fix: test alsc for 'DEBIT' transaction

--- test/test_transaction.py
+++ test/test_transaction.py
@@ -10,15 +10,18 @@ class TestTransaction(unittest.TestCase):
 
     def setUp(self):
         self.credit = Transaction(1000, 'CREDIT')
+        self.debit = Transaction(500, 'DEBIT')
 
     def test_amount(self):
         """should have a property to return the amount"""
         self.assertEqual(self.credit.amount, 1000)
 
-    def test_type_credit(self):
-        """should have a category property of 'CREDIT' when 'CREDIT' passed in'"""
+    def test_category_credit(self):
+        """should have a property of category, which is set to either 'CREDIT' or 'DEBIT'"""
         self.assertEqual(self.credit.amount, 1000)
         self.assertEqual(self.credit.category, 'CREDIT')
+        self.assertEqual(self.debit.amount, 500)
+        self.assertEqual(self.debit.category, 'DEBIT')
 
 
 if __name__ == "__main__":
