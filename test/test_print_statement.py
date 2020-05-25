3a63c133ccd10a8d98d88f98fc2da9859e894437
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-05-25 13:30:27 +0100

    refactor: get_printout method tests to use mock dependencies

--- test/test_print_statement.py
+++ test/test_print_statement.py
@@ -6,8 +6,6 @@ import os.path
 import sys
 sys.path.append(os.path.abspath('.'))
 from lib.print_statement import PrintStatement
-from lib.transaction_history import TransactionHistory
-# from lib.transaction import PrintStatement
 
 
 class TestPrintStatement(unittest.TestCase):
@@ -15,25 +13,30 @@ class TestPrintStatement(unittest.TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_get_printout(self, mock_stdout):
 
-        history = TransactionHistory()
-        history.add(1000, 'CREDIT', 0)
-        history.add(500, 'DEBIT', 1000)
+        credit = MagicMock()
+        amount1 = PropertyMock(return_value=1000)
+        category1 = PropertyMock(return_value='CREDIT')
+        balance1 = PropertyMock(return_value=1000)
+        type(credit).amount = amount1
+        type(credit).category = category1
+        type(credit).balance = balance1
+
+        debit = MagicMock()
+        amount1 = PropertyMock(return_value=250)
+        category1 = PropertyMock(return_value='DEBIT')
+        balance1 = PropertyMock(return_value=750)
+        type(debit).amount = amount1
+        type(debit).category = category1
+        type(debit).balance = balance1
+
+        history = MagicMock()
+        history.get_history.return_value = [credit, debit]
 
         printer = PrintStatement(history)
         printer.get_printout()
-        expected = 'CREDIT: 1000.00, BALANCE: 1000.00\nDEBIT: 500.00, BALANCE: 500.00\n'
+        expected = 'CREDIT: 1000.00, BALANCE: 1000.00\nDEBIT: 250.00, BALANCE: 750.00\n'
         self.assertEqual(mock_stdout.getvalue(), expected)
 
 
 if __name__ == "__main__":
     unittest.main()
-
-    #    credit = MagicMock()
-    #     category1 = PropertyMock(return_value='CREDIT')
-    #     type(credit).category = category1
-    #     print(credit.category)
-
-    #     debit = MagicMock()
-    #     category2 = PropertyMock(return_value='DEBIT')
-    #     type(debit).category = category2
-    #     print(debit.category)
